640f34a22d60cf550e3a14cd8277faaffd0a058e
Kyle Kindt <k.kindt8@gmail.com>
2021-03-12 14:29:57 -0500

    commit tree 52ffc86402fd233b4964ca7c8071df789709d26b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require "pry"
+class School
+
+attr_accessor :roster, :name_of_student, :grade
+
+    def initialize(school)
+        @roster = {}
+    end
+
+    def add_student(name_of_student, grade)
+
+       if @roster.include?(grade)
+        @roster[grade] << name_of_student
+       else 
+        @roster[grade] = [name_of_student]
+       end
+
+       end
+        
+    def grade(grade)
+        @roster[grade]
+        
+    end
+
+    def sort 
+        new_list = {}
+        @roster.each {|name_of_student, grade| new_list[name_of_student] = grade.sort}
+        new_list
+    end
+
+end
